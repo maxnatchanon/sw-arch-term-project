b3ebdc55fa9ec8be417eb2a1ecaa0b7e85886199
Natchanon <natchanon.347@gmail.com>
2019-11-21 22:05:28 +0700

    Add problem service

---
+++ problem-service/problem-service.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const app = express()
+const axios = require('axios')
+
+app.get('/problem', (req, res) => {
+	console.log('Problem service: executing /problem')
+	axios.get('http://localhost:9000/problem')
+		.then(response => {
+			console.log('Problem service: received response from database service')
+			res.send(response.data)
+		})
+		.catch(error => {
+			console.log('Problem service: received error from database service')
+			console.error(error)
+			res.send(error)
+		})
+})
+
+app.get('/checkResult', (req, res) => {
+	console.log('Problem service: executing /checkResult')
+	axios.get('http://localhost:9000/checkResult')
+		.then(response => {
+			console.log('Problem service: received response from database service')
+			res.send(response.data)
+		})
+		.catch(error => {
+			console.log('Problem service: received error from database service')
+			console.error(error)
+			res.send(error)
+		})
+})
+
+app.listen(process.env.PORT, () => {
+  console.log('Start problem service at port ' + process.env.PORT)
+})
