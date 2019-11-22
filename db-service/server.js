1219d6ec25564bfe5d102956c6cfaab6858a27c9
Natchanon <natchanon.347@gmail.com>
2019-11-22 13:03:22 +0700

    Fix database API

--- db-service/server.js
+++ db-service/server.js
@@ -4,9 +4,9 @@ const mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost:27017/softwareArch', { useNewUrlParser: true })
 
 app.use(express.json())
-const users = mongoose.model('users',{username : String,password : String,score : 0})
-const leaderBoards = mongoose.model('leaderBoard',{table : [ {username : String, score : 0}] })
-const problems  = mongoose.model('problem',{problemId : String, question : String, solution : String})
+const users = mongoose.model('users', { username: String, password: String,score: 0 })
+const leaderBoards = mongoose.model('leaderBoard',  {table: [ { username: String, score: 0 }] })
+const problems  = mongoose.model('problem', { problemId: String, question: String, solution: String })
 
 app.get('/', (req, res) => {
   res.json({ message: 'Ahoy!' })
@@ -16,64 +16,58 @@ app.post('/createUser',async (req,res) => {
     const payload = req.body
     console.log(payload)
     if(payload.username != null && payload.password != null) {
-        const findUser = await users.find({username : payload.username})
+        const findUser = await users.find({username: payload.username})
         if(findUser.length > 0 ) {
-            res.status(400).send('Duplicate Username')
+            res.status(400).send({ message: 'This username has been taken' })
         }
         else {
-             const user = new users({username : payload.username,password : payload.password,score : 0})
+             const user = new users({username: payload.username, password: payload.password, score: 0})
              await user.save()
-             res.json("Create Success "+payload.username)
+             res.json('Create Success: ' + payload.username)
 
         }
-    }
-    else {
-        res.status(400).send('Invalid Username or Password')
+    } else {
+        res.status(400).send({ message: 'Invalid Username or Password' })
     }
 })
 
 app.post('/login',async(req,res) => {
     const payload = req.body
-    const findUser = await users.find({username: payload.username,password : payload.password })
-    if(findUser.length > 0){
-        res.json({username : findUser[0].username,score : findUser[0].score})
-    }
-    else {
-       res.status(400).send('Invalid Username or Password')
+    const findUser = await users.find({ username: payload.username,password: payload.password })
+    if( findUser.length > 0){
+        res.json({ username: findUser[0].username,score: findUser[0].score })
+    } else {
+       res.status(400).send({ message: 'Invalid Username or Password' })
     }
 })
 
-app.post('/update',async(req,res) => {
+app.post('/updateScore',async(req,res) => {
     const payload = req.body
     const findUser = await users.find({username: payload.username})
-    if(findUser.length > 0){
+    if (findUser.length > 0) {
         await users.updateOne(
-            {username : payload.username},
+            { username: payload.username },
             {
-                $set : { score : payload.score}
+                $set: { score: payload.score}
             }
         )
         res.json(payload)
-    }
-    else {
-        res.status(400).send('Invalid Username or Password')
+    } else {
+        res.status(400).send({ message: 'Invalid Username or Password' })
     }
 })
 
-app.get('/leaderBoard',(req,res) => {
-    res.json(leaderBoards.find({}))
-}) 
-
-app.get('/problem',(req,res)=> {
+app.get('/problems',(req,res)=> {
     res.json(problems.find({}))
 })
 
 app.get('/scoreLeaderboard', async (req,res) => {
-    const board = await users.find({} ,{username : 1,score :1}).sort({score : 1})
+    const board = await users.find({} ,{ username: 1,score :1 }).sort({ score: 1 })
     console.log(board)
    // delete
     res.json(board)
 })
+
 app.listen(9000, () => {
   console.log('Application is running on port 9000')
 })
