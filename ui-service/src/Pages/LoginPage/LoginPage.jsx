6b11200a6636d40062cd76762814cec53c8f5c34
Natchanon <natchanon.347@gmail.com>
2019-11-23 23:56:51 +0700

    Add main page

--- ui-service/src/Pages/LoginPage/LoginPage.jsx
+++ ui-service/src/Pages/LoginPage/LoginPage.jsx
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import ServiceManager from '../../Services/ServiceManager';
+import history from '../../history';
 import './LoginPage.css';
 
 class LoginPage extends Component {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
         this.state = {
-            username: "",
-            password: "",
+            username: '',
+            password: '',
         };
     }
 
@@ -22,8 +23,13 @@ class LoginPage extends Component {
             alert('Please enter your username and password');
             return;
         }
-        ServiceManager.login(this.state, (user, error) => {
-
+        ServiceManager.login(this.state, (user, errorMessage) => {
+            if (user) {
+                this.props.setUser(user)
+                history.push('/main')
+            } else if (errorMessage) {
+                alert(errorMessage)
+            }
         })
     }
 
@@ -32,15 +38,22 @@ class LoginPage extends Component {
             alert('Please enter your username and password');
             return;
         }
-        
+        ServiceManager.register(this.state, (user, errorMessage) => {
+            if (user) {
+                this.props.setUser(user)
+                history.push('/main')
+            } else if (errorMessage) {
+                alert(errorMessage)
+            }
+        })
     }
 
     render() {
         return (
             <div className='login-page-container'>
                 <div>
-                    <input name='username' placeholder='Username' onChange={ (e) => this.handleInputChange(e) }/>
-                    <input name='password' placeholder='Password' onChange={ (e) => this.handleInputChange(e) }/>
+                    <input name='username' placeholder='Username' onChange={(e) => this.handleInputChange(e)}/>
+                    <input name='password' placeholder='Password' onChange={(e) => this.handleInputChange(e)}/>
                     <div>
                         <button onClick={this.handleLoginButton}>Log in</button>
                         <button onClick={this.handleRegisterButton}>Register</button>
---
+++ ui-service/src/Pages/MainPage/MainPage.jsx
@@ -0,0 +1,144 @@
+import React, { Component } from 'react';
+import ServiceManager from '../../Services/ServiceManager';
+import history from '../../history';
+import './MainPage.css';
+
+class MainPage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            user: {
+                username: '',
+                score: 0,
+            },
+            answer: '',
+            problem: {
+                problemId: '',
+                question: '',
+            },
+            leaderboard: [],
+            showResultText: false,
+            correct: false,
+        };
+    }
+
+    componentDidMount = () => {
+        if (!this.props.user) {
+            history.push('/');
+        } else {
+            this.setState({
+                user: this.props.user,
+            });
+            this.getProblem();
+            this.getLeaderboard();
+            setInterval(this.getLeaderboard, 1000);
+        }
+    }
+
+    handleLogOut = () => {
+        this.props.setUser(undefined);
+        history.push('/');
+    }
+
+    getProblem = () => {
+        ServiceManager.getProblem((problem, errorMessage) => {
+            if (errorMessage) {
+                alert(errorMessage);
+            } else {
+                this.setState({
+                    problem: problem,
+                });
+            }
+        });
+    }
+
+    getLeaderboard = () => {
+        ServiceManager.getLeaderboard((leaderboard, errorMessage) => {
+            if (errorMessage) {
+                alert(errorMessage);
+            } else {
+                this.setState({
+                    leaderboard: leaderboard,
+                });
+            }
+        })
+    }
+
+    handleAnswerChange = (e) => {
+        this.setState({
+            answer: e.target.value,
+        });
+    }
+
+    handleSubmit = () => {
+        ServiceManager.checkResult(
+            { problemId: this.state.problem.problemId, answer: this.state.answer },
+            (correct, errorMessage) => {
+                if (errorMessage) {
+                    alert(errorMessage);
+                } else {
+                    this.setState({
+                        showResultText: true,
+                        correct: correct,
+                    });
+                    if (correct) {
+                        var user = this.state.user;
+                        user.score += 1;
+                        this.setState({
+                            user: user,
+                            answer: '',
+                        });
+                        this.getProblem()
+                    } else {
+                        alert('FALSE');
+                    }
+                }
+            }
+        )
+    }
+
+    render() {
+        return (
+            <div className='main-page-container'>
+                <div className='main-page-header-container'>
+                    <div className='main-page-header-user-section'>
+                        <p>Logged in as <span>{this.state.user.username}</span></p>
+                        <p>Your score: <span>{this.state.user.score}</span></p>
+                    </div>
+                    <button onClick={this.handleLogOut}>Log out</button>
+                </div>
+                <div className='main-page-content-container'>
+                    <div className='main-page-problem-section'>
+                        <div>
+                            <span>{this.state.problem.question}&emsp;_________________</span>
+                            <input value={this.state.answer} onChange={(e) => this.handleAnswerChange(e)}/>
+                            <button onClick={this.handleSubmit}>SUBMIT</button>
+                            <div
+                            style={{color: this.state.correct ? 'green' : 'red', opacity: this.state.showResultText ? '1' : '0'}}
+                            >
+                                {this.state.correct ? 'Correct!' : 'Incorrect!'}
+                            </div>
+                        </div>
+                    </div>
+                    <div className='main-page-leaderboard-section'>
+                        <div className='main-page-leaderboard-header'>Leaderboard</div>
+                        {this.state.leaderboard.map((user, index) => 
+                            <div
+                            className={
+                                this.props.user && user.username === this.props.user.username ? 
+                                    'main-page-leaderboard-user-me' : 
+                                    'main-page-leaderboard-user'
+                            }
+                            >
+                                <span>{index + 1}. {user.username}</span>
+                                <span>{user.score}</span>
+                            </div>
+                        )}
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default MainPage;
