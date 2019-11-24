429f198f69bd413666eb64da435b6a0991646059
Akamate <aukmate@gmail.com>
2019-11-24 21:27:21 +0700

    fix bug

--- problem-service/problem-service.js
+++ problem-service/problem-service.js
@@ -1,26 +1,29 @@
 const express = require('express')
 const app = express()
 const axios = require('axios')
-
+app.use(express.json())
 var problems = []
 
-app.get('/problem', (req, res) => {
-	console.log('Problem service: executing /problem')
+app.get('/problems', (req, res) => {
+	console.log('Problem service: executing /problems')
 	if (problems.length === 0) {
 		res.status(500).send({ message: 'Cannot receive problems from database' })
 	} else {
 		var problem = problems[Math.floor(Math.random() * problems.length)]
-		delete problem.solution
-		res.send(problem)
+		var result = Object.assign({},problem)
+		delete result.solution
+		res.json(result)
 	}
 })
 
-app.get('/checkResult', (req, res) => {
-	console.log('Problem service: executing /checkResult')
-	const problem = problems.find(problem => problem.problemId === req.query.problemId)
+app.get('/checkResult', async (req, res) => {
+	console.log('Problem service: executing /checkResult' + req.query.problemId)
+	const problem = await problems.find(problem => problem.problemId === req.query.problemId)
+	console.log(problem)
 	if (problems === undefined) {
 		res.status(500).send({ message: 'Problem not found' })
 	} else {
+		console.log(problem.solution)
 		res.send(problem.solution === req.query.answer)
 	}
 })
