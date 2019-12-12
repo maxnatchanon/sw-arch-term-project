6c33ee7ed844b7a7cb238d89445d0d1390564a37
Akamate <aukmate@gmail.com>
2019-12-12 14:26:08 +0700

    make loadbalance and add service path

--- gateway/gateway.js
+++ gateway/gateway.js
@@ -4,24 +4,54 @@ const axios = require('axios')
 
 app.use(express.json())
 
+var services = [[],[],[]]
+var serviceName = ['ProblemService','UserService','LeaderboardService']
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+function randomIndex(serviceNumber){
+    var randomNumber = (parseInt(Math.random()*100))%services[serviceNumber].length
+    console.log('numofPort = ',services[serviceNumber].length)
+    return randomNumber
+}
+
 app.get('/problems', (req, res) => {
-    var port = (Math.random()<0.5) ? (8080) : (8081);
-	console.log('Call Problem service: executing /problems at port='+port)
-    axios.get('http://localhost:'+port+'/problems')
+    getProblems(res)
+})
+
+async function  getProblems(res) {
+    var index = randomIndex(0)
+    var port = services[0][index]
+
+    console.log('Call Problem service: executing /problems at port='+port + 'at index='+index)
+    await axios.get('http://localhost:'+port+'/problems')
     .then( response => {
         console.log(response.data)
         res.json(response.data);
     })
     .catch(error => {
-        console.log(error.response.status);
-        res.status(error.response.status).send(error.response.data)
+        services[0].splice(index,1)
+        if(services[0].length == 0)
+             res.status(error.response.status).send(error.response.data)
+        else {
+            getProblems(res)
+        }
     })
+}
+
+app.get('/checkResult', async(req, res) => {
+    checkResult(req,res)
 })
 
-app.get('/checkResult', (req, res) => {
-    var port = (Math.random()<0.5) ? (8080) : (8081);
-	console.log('Call Problem service: executing /checkResult at port='+port)
-    axios.get('http://localhost:'+port+'/checkResult', {
+async function checkResult(req,res){
+    var index = randomIndex(0)
+    var port = services[0][index]
+    console.log('Call Problem service: executing /checkResult at port='+port)
+    await axios.get('http://localhost:'+port+'/checkResult', {
         params: {
           problemId : req.query.problemId,
           answer : req.query.answer
@@ -31,61 +61,114 @@ app.get('/checkResult', (req, res) => {
         res.send(response.data);
     })
     .catch(error => {
-        console.log(error.response.status);
-        res.status(error.response.status).send(error.response.data)
+        services[0].splice(index,1)
+        if(services[0].length == 0)
+             res.status(error.response.status).send(error.response.data)
+        else {
+            checkResult(req,res)
+        }
     })
-})
+}
 
 app.post('/register', (req, res) => {
-	console.log('Call User service: executing /register')
-	const payload = req.body
-    axios.post('http://localhost:7000/register',payload)
+    register(req,res)
+})
+
+async function register(req,res) {
+    var index = randomIndex(1)
+    var port = services[1][index]
+    console.log('Call User service: executing /register at Port='+port)
+    const payload = req.body
+    await axios.post('http://localhost:'+port+'/register',payload)
     .then( response => {
         console.log(response.data)
         res.send(response.data);
     })
     .catch(error => {
-        console.log(error.response.status);
-        res.status(error.response.status).send(error.response.data)
+        services[1].splice(index,1)
+        if(services[1].length == 0)
+             res.status(error.response.status).send(error.response.data)
+        else {
+            register(req,res)
+        }
     })
-})
+}
 
 app.post('/login', (req, res) => {
-	console.log('Call User service: executing /login')
-	const payload = req.body
-    axios.post('http://localhost:7000/login',payload)
+	login(req,res)
+})
+
+async function login(req,res) {
+    var index = randomIndex(1)
+    var port = services[1][index]
+    console.log('Call User service: executing /login at Port=',port)
+    const payload = req.body
+    await axios.post('http://localhost:'+port+'/login',payload)
     .then( response => {
         res.send(response.data);
     })
     .catch(error => {
-        console.log(error.response.status);
-        res.status(error.response.status).send(error.response.data)
+        services[1].splice(index,1)
+        if(services[1].length == 0)
+             res.status(error.response.status).send(error.response.data)
+        else {
+            login(req,res)
+        }
     })
-})
+}
 
 app.post('/updateScore', (req, res) => {
-	console.log('Call Leaderboard service: executing /updateScore')
-	const payload = req.body
-    axios.post('http://localhost:6000/updateScore',payload)
-    .then( response => {
-        res.send(response.data);
-    })
-    .catch(error => {
-        console.log(error.response.status);
-        res.status(error.response.status).send(error.response.data)
-    })
+	updateScore(req,res)
 })
 
-app.get('/scoreLeaderboard', (req, res) => {
-	console.log('Call Leaderboard service: executing /scoreLeaderboard')
-    axios.get('http://localhost:6000/scoreLeaderboard')
+async function updateScore(req,res){
+    var index = randomIndex(2)
+    var port = services[2][index]
+    console.log('Call Leaderboard service: executing /updateScore')
+    const payload = req.body
+    await axios.post('http://localhost:'+port+'/updateScore',payload)
     .then( response => {
         res.send(response.data);
     })
     .catch(error => {
-        console.log(error.response.status);
-        res.status(error.response.status).send(error.response.data)
+        services[2].splice(index,1)
+        if(services[2].length == 0)
+             res.status(error.response.status).send(error.response.data)
+        else {
+            updateScore(req,res)
+        }
     })
+}
+
+app.get('/scoreLeaderboard', (req, res) => {
+	scoreLeaderboard(res)
+})
+
+async function scoreLeaderboard(res) {
+    var index = randomIndex(2)
+    var port = services[2][index]
+    console.log('Call Leaderboard service: executing /scoreLeaderboard')
+    await axios.get('http://localhost:6000/scoreLeaderboard')
+    .then( response => {
+        res.send(response.data);
+    })
+    .catch(error => {
+        services[2].splice(index,1)
+        if(services[2].length == 0){
+             console.log(error.response)
+             res.status(error.response.status).send(error.response.data)
+        }
+        else {
+            updateScore(req,res)
+        }
+    })
+}
+
+app.post('/addService',async(req,res) => {
+    const payload = req.body
+    console.log('Add ' + serviceName[payload.serviceNumber]+'  at Port '+payload.servicePort)
+    await services[payload.serviceNumber].push(payload.servicePort)
+    res.send('successfully')
 })
 
 var server = app.listen(5000, function () {
